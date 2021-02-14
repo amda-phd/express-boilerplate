5736b45b3a7489e7754c11e4322882a3d8156c34
amda <amda.prog@gmail.com>
2021-02-14 21:15:31 +0100

    Added some Express middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
+const errorhandler = require("errorhandler");
 
 const health = require("./routers/health");
 
 const app = express();
 app.use(express.json());
+app.use(
+  morgan(
+    process.env.NODE_ENV == "dev"
+      ? ":method :url :status :response-time ms - :res[content-length]"
+      : "tiny"
+  )
+);
+if (process.env.NODE_ENV) {
+  app.use(errorhandler());
+}
 
 app.use(health);
 
