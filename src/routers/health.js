8ace28d5dfc8917ef3b395c2a4fb9d624be715aa
amda <amda.prog@gmail.com>
2021-02-18 19:18:46 +0100

    Tests boilerplate added

--- src/routers/health.js
+++ src/routers/health.js
@@ -3,7 +3,11 @@ const { Router } = require("express");
 const router = new Router();
 
 router.get("/health", (req, res) => {
-  return res.send("Healthy!");
+  return res.send({ message: "Healthy!" });
+});
+
+router.get("/ping", (req, res) => {
+  return res.send({ ping: "pong" });
 });
 
 module.exports = router;
