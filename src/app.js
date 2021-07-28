5dd5f9955d3967911225b457a38ce5063789c055
amda <amda.prog@gmail.com>
2021-07-28 17:03:41 +0200

    Added module aliases for package and jest

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const errorhandler = require("errorhandler");
 
-const health = require("./routers/health");
+const health = require("Routers/health");
 
 const app = express();
 app.use(express.json());
