6142a590a695f9971a29276a053ac8532a7f394b
amda <amda.prog@gmail.com>
2021-07-29 14:12:01 +0200

    Added basic validator

---
+++ src/validators/health.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+
+module.exports = {
+  query: validator.query(
+    Joi.object({
+      api: Joi.boolean(),
+    })
+  ),
+};
