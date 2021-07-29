e92a360a4735a0c59ba8eef5bfbee9a7899c6b52
amda <amda.prog@gmail.com>
2021-07-29 14:09:26 +0200

    Richer test suites includes /health queries, errors and database

--- tests/health.test.js
+++ tests/health.test.js
@@ -1,18 +1,38 @@
 const request = require("supertest");
 
 const app = require("@app");
+const Health = require("Models/health");
 
-// SUCCESS
-describe("API should", () => {
-  it("be healthy", async () => {
-    const response = await request(app).get("/health").expect(200);
-
-    expect(response.body.message).toBeDefined();
+describe("/health", () => {
+  it("Returns empty object when called without query", (done) => {
+    request(app).get("/health").expect(200, {}, done());
   });
 
-  it("ping", async () => {
-    const response = await request(app).get("/ping").expect(200);
+  describe("?api=true", () => {
+    it("Checks only API's status", (done) => {
+      request(app).get("/health?api=true").expect(200, { api: true }, done());
+    });
 
-    expect(response.body.ping).toBeDefined();
+    describe("&db=true", () => {
+      it("Also checks the database's status", async (done) => {
+        await request(app)
+          .get("/health?api=true&db=true")
+          .expect(200, { api: true, db: true });
+        const health = await Health.findAll();
+        expect(health[0].isHealthy).toBeTruthy(done());
+      });
+    });
+  });
+
+  describe("Unexpected query", () => {
+    it("Returns coherent error", (done) => {
+      request(app).get("/health?caca=true").expect(400, done());
+    });
+  });
+});
+
+describe("/ping", () => {
+  it("pongs", (done) => {
+    request(app).get("/ping").expect(200, { ping: "pong" }, done());
   });
 });
